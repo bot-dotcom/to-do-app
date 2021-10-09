fffd4d67e4483d8ed433220ecab605e247ce6580
dejansev <dejansdev@gmail.com>
2021-10-09 21:22:47 +0200

    Small changes in app.js

--- app.js
+++ app.js
@@ -21,8 +21,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Handle HTTP POST requests
 app.use(express.urlencoded({ extended: true }));
 
-// 
-app.use(express.json());
-
 // Application routes
 app.use(taskRoutes);
