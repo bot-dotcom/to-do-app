a07a96b806924669db9748b2f29771eb3174e23e
dejansev <dejansdev@gmail.com>
2021-10-11 18:26:28 +0200

    Using parameterized queries

--- models/taskModels.js
+++ models/taskModels.js
@@ -3,7 +3,7 @@ const database = require('../database/database');
 
 // Get all tasks from database
 const getTasks = (callback) => {
-  const sql = `SELECT * FROM Tasks`;
+  sql = `SELECT * FROM Tasks`;
   database.appDatabase.all(sql, [], (error, rows) => {
     if (error) {
       callback(error);
@@ -14,8 +14,8 @@ const getTasks = (callback) => {
 
 // Create new task
 const createTask = (task, status, callback) => {
-  const sql = `INSERT INTO Tasks (Task, Status) VALUES ('${task}', '${status}')`;
-  database.appDatabase.run(sql, [], (error, row) => {
+  const sql = `INSERT INTO Tasks (Task, Status) VALUES (?, ?)`;
+  database.appDatabase.run(sql, [task, status], (error, row) => {
     if (error) {
       callback(error.message);
     }
@@ -26,8 +26,8 @@ const createTask = (task, status, callback) => {
 
 // Get task
 const getTask = (id, callback) => {
-  const sql = `SELECT * FROM Tasks WHERE Task_Id = ${id}`;
-  database.appDatabase.get(sql, [], (error, row) => {
+  const sql = `SELECT * FROM Tasks WHERE Task_Id = ?`;
+  database.appDatabase.get(sql, [id], (error, row) => {
     if (error) {
       callback(error.message);
     }
@@ -37,8 +37,8 @@ const getTask = (id, callback) => {
 
 // Delete task  
 const deleteTask = (id, callback) => {
-  const sql = `DELETE FROM Tasks WHERE Task_Id = ${id}`;
-  database.appDatabase.run(sql, [], (error, row) => {
+  const sql = `DELETE FROM Tasks WHERE Task_Id = ?`;
+  database.appDatabase.run(sql, [id], (error, row) => {
     if (error) {
       callback(error.message);
     }
@@ -49,8 +49,8 @@ const deleteTask = (id, callback) => {
 
 // Update task  
 const updateTask = (task, status, id, callback) => {
-  let sql = `UPDATE Tasks SET Task = '${task}', Status = '${status}' WHERE (Task_ID = ${id})`;
-  database.appDatabase.run(sql, [], (error, row) => {
+  let sql = `UPDATE Tasks SET Task = ?, Status = ? WHERE Task_ID = ?`;
+  database.appDatabase.run(sql, [task, status, id], (error, row) => {
     if (error) {
       callback(error.message);
     }
